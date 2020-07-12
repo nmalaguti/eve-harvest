299d581996d7ddd94c9ea675bdbb87fc99eb0892
Nick Malaguti <nmalaguti@users.noreply.github.com>
2020-07-12 11:58:30 -0400

    Initial site

---
+++ precache-manifest.585c3ca98f9dae9b2cd9d286d57dc5b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "240d3cc6996d518869f279fcf9320c0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1daa3750815c95fa940b",
+    "url": "/static/css/main.38078638.chunk.css"
+  },
+  {
+    "revision": "7a1defe49da3d4ca9cdb",
+    "url": "/static/js/2.0c4d6ff5.chunk.js"
+  },
+  {
+    "revision": "1daa3750815c95fa940b",
+    "url": "/static/js/main.36524afd.chunk.js"
+  },
+  {
+    "revision": "aa1d0f1c672be7083a94",
+    "url": "/static/js/runtime~main.afeea630.js"
+  }
+]);
